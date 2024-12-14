c755be23ca485b84c8285ed735afdff5e32e5008
Sapegin-RB <sapeginskal@mail.ru>
2024-12-14 20:14:17 +0700

    translation into Russian

--- random_quote_app/app.py
+++ random_quote_app/app.py
@@ -1,21 +1,33 @@
 from flask import Flask, render_template, jsonify
 import requests
+from googletrans import Translator
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # Отображение HTML-страницы
+    # Отображение HTML-страницы
+    return render_template('index.html')
 
 @app.route('/api/get-quote', methods=['GET'])
 def get_quote():
     try:
+        # Запрос к API для получения случайной цитаты
         response = requests.get('https://zenquotes.io/api/random')
         if response.status_code == 200:
             data = response.json()
+            quote = data[0]["q"]  # Текст цитаты
+            author = data[0]["a"]  # Автор цитаты
+
+            # Перевод цитаты и автора на русский
+            translator = Translator()
+            translated_quote = translator.translate(quote, src='en', dest='ru').text
+            translated_author = translator.translate(author, src='en', dest='ru').text
+
+            # Возвращаем переведенные данные в формате JSON
             return jsonify({
-                "quote": data[0]["q"],
-                "author": data[0]["a"]
+                "quote": translated_quote,
+                "author": translated_author
             })
         else:
             return jsonify({"error": "Failed to fetch quote from API"}), 500
